85e736e02c8eeda3b795aecb84f1e364913f0994
Ádám Kovács <vartefol@gmail.com>
2023-05-14 20:05:10 +0200

    fix: update root readme generation

--- src/files/readme.md.ts
+++ src/files/readme.md.ts
@@ -1,7 +1,9 @@
 import { Settings } from '../types.js';
-import { isSharedType } from '../utils.js';
+import { isRepoOnGitHub, isSharedType } from '../utils.js';
 
 export function getReadmeMdContent(settings: Settings): string {
+	const isGitUrl = isRepoOnGitHub(settings.repoUrl) && settings.repoUrl.endsWith('.git');
+	const repoName = isGitUrl ? settings.repoUrl.split('/').pop()?.split('.git')[0] : undefined;
 	return `# @${settings.client}/monorepo
 
 ## Workplaces
@@ -21,8 +23,8 @@ ${settings.archetypes
 
 ## Install
 
-1. Clone the repository
-1. \`cd\` into the folder
+1. ${isGitUrl ? `\`git clone ${settings.repoUrl}\`` : 'Clone the repository'}
+1. ${repoName ? `\`cd ${repoName}\`` : '`cd` into the folder'}
 1. \`pnpm install\`
 1. \`pnpm dev\` _(or you can find more specific commands in the root
 		\`package.json\`)_
